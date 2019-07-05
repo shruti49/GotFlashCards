9d07af3543e3e14f73e88e2408f068b543429a4c
Shruti <shrutishastri39@gmail.com>
2019-07-05 18:58:22 +0530

    Updates

---
+++ precache-manifest.c0a3c2cc7b81579e2a26504b62007eab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a91ddf8601a2d76f7093a2cdaad4ab",
+    "url": "/GotFlashCards/index.html"
+  },
+  {
+    "revision": "6f6d9243dc53ff0713d5",
+    "url": "/GotFlashCards/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ffd6c2435a28d347f2d2",
+    "url": "/GotFlashCards/static/css/main.49654e0d.chunk.css"
+  },
+  {
+    "revision": "6f6d9243dc53ff0713d5",
+    "url": "/GotFlashCards/static/js/2.2d717d87.chunk.js"
+  },
+  {
+    "revision": "ffd6c2435a28d347f2d2",
+    "url": "/GotFlashCards/static/js/main.30a63c0c.chunk.js"
+  },
+  {
+    "revision": "04234175d15748e59c2c",
+    "url": "/GotFlashCards/static/js/runtime~main.147c7a1a.js"
+  }
+]);
